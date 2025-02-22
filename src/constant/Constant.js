66a32594c4ebe7775b7b7fd319238250ab16f1b7
Ibrahim Khan <155100045+Ibrahimkhan432@users.noreply.github.com>
2025-02-22 13:06:52 +0500

    constant

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -1,7 +1,7 @@
 const devURL = "http://localhost:4000";
 const prodURL = "https://carpool-backend-staging.up.railway.app";
 
-export const BASE_URL = devURL;
+export const BASE_URL = prodURL;
  
 export const AppRoutes = {
   signupUser: BASE_URL + "/user/signupUser",
